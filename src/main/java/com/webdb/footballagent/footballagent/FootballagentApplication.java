378673f4e39b251ac092410a6f8841f859a89114
Sanskar Gupta <snskr95@gmail.com>
2021-11-15 15:50:11 +0100

    initialized spring boot project

---
+++ src/main/java/com/webdb/footballagent/footballagent/FootballagentApplication.java
@@ -0,0 +1,13 @@
+package com.webdb.footballagent.footballagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballagentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballagentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webdb/footballagent/footballagent/FootballagentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webdb.footballagent.footballagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballagentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
