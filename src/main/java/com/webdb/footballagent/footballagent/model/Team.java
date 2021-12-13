fc7dab2f825a09553d26f249f5dc715d63da0d45
Sanskar Gupta <snskr95@gmail.com>
2021-12-13 19:19:04 +0100

    added team utl and team link fields

--- src/main/java/com/webdb/footballagent/footballagent/model/Team.java
+++ src/main/java/com/webdb/footballagent/footballagent/model/Team.java
@@ -10,6 +10,8 @@ public class Team {
 
      @Field
      private String team;
+     private String team_logo;
+     private String team_link;
 //    private String name;
 //    private  String code;
 //    private Titles titles;
@@ -38,6 +40,22 @@ public class Team {
 
     public void setPlayers(List<Player> players) {
         this.players = players;
+    }
+
+    public String getTeam_logo() {
+        return team_logo;
+    }
+
+    public void setTeam_logo(String team_logo) {
+        this.team_logo = team_logo;
+    }
+
+    public String getTeam_link() {
+        return team_link;
+    }
+
+    public void setTeam_link(String team_link) {
+        this.team_link = team_link;
     }
 }
 
